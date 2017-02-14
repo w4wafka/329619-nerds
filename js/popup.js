52fe0a20ca387880466ff4898ad9210dd5178ed5
w4wafka <ofunduk4@gmail.com>
2017-02-14 22:39:28 +0500

    JS, небольшие правки

--- js/popup.js
+++ js/popup.js
@@ -3,34 +3,88 @@ var modal = document.querySelector(".write-us");
 var overlay = document.querySelector(".overlay");
 var close = document.querySelector(".write-us-close");
 var send = document.querySelector(".write-us-btn");
-var name = document.querySelector(".text-name");
-var eMail = document.querySelector(".text-email");
-var letter = document.querySelector(".text-letter");
-var form = document.querySelector(".write-us-form");
+var name_form = document.querySelector(".text-name");
+var eMail_form = document.querySelector(".text-email");
+var letter_form = document.querySelector(".text-letter");
+var form_modal = document.querySelector(".write-us-form");
+var close_popup = function(event) {
+  if (name_form.classList.contains("none-value")) {
+      name_form.classList.remove("none-value");
+    }
+if (eMail_form.classList.contains("none-value")) {
+      eMail_form.classList.remove("none-value");
+    }
+if (letter_form.classList.contains("none-value")) {
+      letter_form.classList.remove("none-value");
+    }}
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
+  modal.classList.remove("js-animation-modal");
   modal.classList.add("modal-shown");
-  overlay.classList.add("modal-shown");
+  overlay.classList.remove("js-animation-overlay");
+  overlay.classList.add("overlay-shown");
 });
 
 close.addEventListener("click", function(event) {
   event.preventDefault();
   modal.classList.remove("modal-shown");
-  overlay.classList.remove("modal-shown");
+  modal.classList.add("js-animation-modal");
+  overlay.classList.remove("overlay-shown");
+  overlay.classList.add("js-animation-overlay");
+  if (modal.classList.contains("error")) {
+    modal.classList.remove("error");
+  }
+  close_popup();
 });
 
 overlay.addEventListener("click", function(event) {
   event.preventDefault();
   modal.classList.remove("modal-shown");
-  overlay.classList.remove("modal-shown");
+  modal.classList.add("js-animation-modal");
+  overlay.classList.remove("overlay-shown");
+  overlay.classList.add("js-animation-overlay");
+  close_popup();
+  if (modal.classList.contains("error")) {
+    modal.classList.remove("error");
+  }
 });
 
-form.addEventListener("submit", function(event) {
-  if (!name.value || !eMail.value || !letter.value) {
-    event.preventDefault();
-    name.classList.add("none-value");
-    eMail.classList.add("none-value");
-    letter.classList.add("none-value");
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (modal.classList.contains("modal-shown") || overlay.classList.contains("overlay-shown")) {
+      modal.classList.remove("modal-shown");
+      modal.classList.add("js-animation-modal");
+      overlay.classList.remove("overlay-shown");
+      overlay.classList.add("js-animation-overlay");
+      close_popup();
+      if (modal.classList.contains("error")) {
+        modal.classList.remove("error");
+      }
+    }
   }
+})
+
+form_modal.addEventListener("submit", function(event) {
+  if (!name_form.value) {
+    event.preventDefault();
+    name_form.classList.add("none-value");
+    modal.classList.add("error");
+  } else { if (name_form.classList.contains("none-value")) {
+        name_form.classList.remove("none-value");
+      }};
+  if (!eMail_form.value) {
+    eMail_form.classList.add("none-value");
+    event.preventDefault();
+    modal.classList.add("error");
+  } else { if (eMail_form.classList.contains("none-value")) {
+      eMail_form.classList.remove("none-value");
+  }}
+  if (!letter_form.value) {
+    letter_form.classList.add("none-value");
+    event.preventDefault();
+    modal.classList.add("error");
+  } else { if (letter_form.classList.contains("none-value")) {
+    letter_form.classList.remove("none-value");
+  }}
 });
